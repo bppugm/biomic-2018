e47979ce2e648c578eaa5ff5bd88d9c623e6615c
Rafieiy <davieiycode@users.noreply.github.com>
2018-04-10 09:43:19 +0700

    add g analytics

--- javascripts/all.js
+++ javascripts/all.js
@@ -3536,3 +3536,14 @@ var Popover = function ($) {
 }(jQuery);
 
 }();
+
+  /**
+   * ------------------------------------------------------------------------
+   * Google Analytics
+   * ------------------------------------------------------------------------
+   */
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-116679781-1');
